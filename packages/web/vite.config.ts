54b47b867269e8459c36e2dfe97606a86d7fd8c3
ramezj <ramezjoseph8@gmail.com>
2026-06-22 18:09:16 +0300

    added nitro back!

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,15 +5,15 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     devtools(),
     tailwindcss(),
-    tanstackStart({
-      nitro: { rollupConfig: { external: [/^@sentry\//] } },
-    }),
+    tanstackStart(),
     viteReact(),
   ],
   server: {
